b4fb91de309a69b8e82a6a38a7e233d996e461bb
Jyotiswaruptripathy <64881323+Jyotiswaruptripathy@users.noreply.github.com>
2020-05-06 09:04:20 +0530

    spect_prob5

---
+++ prob 5.py
@@ -0,0 +1,15 @@
+n=int(input('enter the no  :'))
+for i in range (2,n-1):
+    if (n%i) == 0:
+        print('not a prime no.')
+        break
+else:
+    print(' is a prime no.')
+    
+    
+    
+        
+    
+    
+
+    
