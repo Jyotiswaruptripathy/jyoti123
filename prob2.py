ab42d0d4fa6c9a2a24318a81eeb3e64e209624df
Jyotiswaruptripathy <64881323+Jyotiswaruptripathy@users.noreply.github.com>
2020-05-06 09:00:12 +0530

    spect_prob2

---
+++ prob2.py
@@ -0,0 +1,10 @@
+n=int(input('enter the no. of integer u want to add:'))
+sum=0
+
+for k in range (0,n):
+    k=int(input('enter the no.: '))
+    sum=sum+k
+    
+    
+print('the sum of digit is = ',sum)
+
