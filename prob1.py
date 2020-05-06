8ae2009a7f9f4693cd1253374f3728f572c9c0d2
Jyotiswaruptripathy <64881323+Jyotiswaruptripathy@users.noreply.github.com>
2020-05-06 08:58:17 +0530

    spect_prob1

---
+++ prob1.py
@@ -0,0 +1,10 @@
+n1=int(input('enter the 1st no. :'))
+n2=int(input('enter the second no.'))
+
+c=n1*n2
+
+if c<1000:
+    print('the multiplication is',c)
+    
+else:
+    print('the summation is',(n1+n2))
